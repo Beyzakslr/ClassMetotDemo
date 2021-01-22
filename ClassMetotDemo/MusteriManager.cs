d08736b12458ce27d4e43978d96f59352c555c39
Beyza Kesler <beyzakslr@hotmail.com>
2021-01-22 22:54:20 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(" Müşteri başarıyla eklenmiştir");
+
+        }
+
+        public void Ekle2(string Adı, string Soyadı , int Id)
+        {
+            Console.WriteLine("Müşteri başarıyla çıkarıldı");
+        }
+       
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adı = "İrem";
+            musteri1.Soyadı = "Kara";
+            musteri1.Id = 21;
+
+           
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adı = "Birgül";
+            musteri2.Soyadı = "Altındaş";
+            musteri2.Id = 21;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adı);
+                Console.WriteLine(musteri.Soyadı);
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine("-----");
+            }
+
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            Console.WriteLine("-----");
+            musteriManager.Ekle(musteri2);
+            Console.WriteLine("-----");
+
+        }
+    }
+}
